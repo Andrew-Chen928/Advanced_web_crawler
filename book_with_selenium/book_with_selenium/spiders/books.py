48f1a50408d7f48c27a3301d046721aaeda9989b
Andrew-Chen928 <ichiro710928@gmail.com>
2017-02-03 15:44:50 -0800

    adding book_store scrapy with selenium

---
+++ book_with_selenium/book_with_selenium/spiders/books.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from scrapy import Spider
+from selenium import webdriver
+from scrapy.selector import Selector
+from scrapy.http import Request
+from time import sleep
+from selenium.common.exceptions import NoSuchElementException
+
+class BooksSpider(Spider):
+    name = 'books'
+    allowed_domains = ["books.toscrape.com"]
+
+    def start_requests(self):
+        self.driver = webdriver.Chrome('/Users/chianchen/WebCrawler/Python_web_scrapier/chromedriver')
+        self.driver.get('http://books.toscrape.com')
+
+        sel = Selector(text=self.driver.page_source)
+        books = sel.xpath('//h3/a/@href').extract()
+        for book in books:
+        	url = 'http://books.toscrape.com/' + book
+        	yield Request(url, callback=self.parse_book)
+
+        while True:
+            try:
+                next_page = self.driver.find_element_by_xpath('//a[text()="next"]')
+                sleep(3)
+                self.logger.info('sleeping for 3 seconds.')
+                next_page.click()
+
+
+                sel = Selector(text=self.driver.page_source)
+                books = sel.xpath('//h3/a/@href').extract()
+                for book in books:
+        	        url = 'http://books.toscrape.com/catalogue/' + book
+        	        yield Request(url, callback=self.parse_book)
+
+            except NoSuchElementException:
+                self.logger.info('No more pages to load.')
+                self.driver.quit()
+                break  
+
+    def parse_book(self, response):
+        pass
+
+
+    
